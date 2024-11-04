54c8458ad11c44c90cccbd4f4e47e99b7561b76f
Annapaola Maiellaro <maiellaro19@gmail.com>
2024-11-04 15:18:53 +0100

    Initial Commit

---
+++ MyAmazinGaPP/MyAmazinGaPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAmazinGaPPApp.swift
+//  MyAmazinGaPP
+//
+//  Created by Annapaola Maiellaro on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MyAmazinGaPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
